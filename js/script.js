735830de70604d45d3c6580c54c84c32abcd9159
Mark Pinto <mark.pinto.94@gmail.com>
2025-01-29 22:58:02 -0500

    recaptcha client side additions

---
+++ js/script.js
@@ -0,0 +1,38 @@
+let isRecaptchaValidated = false;
+
+function toggleRecaptchaFormMessage(type = "error", hide = false) {
+  document.getElementById(`recaptcha-form-${type}`).style.display = hide
+    ? "none"
+    : "inherit";
+}
+
+function onRecaptchaSuccess() {
+  isRecaptchaValidated = true;
+}
+
+function onRecaptchaError() {
+  toggleRecaptchaFormMessage("error");
+  toggleRecaptchaFormMessage("success", true);
+}
+
+function onRecaptchaResponseExpiry() {
+  onRecaptchaError();
+}
+
+window.onload = function () {
+  const recaptchaForm = document.getElementById("recaptcha-form");
+  recaptchaForm.addEventListener("submit", function (e) {
+    e.preventDefault();
+
+    // failure
+    if (!isRecaptchaValidated) {
+      toggleRecaptchaFormMessage("error");
+      toggleRecaptchaFormMessage("success", true);
+      return;
+    }
+
+    // success
+    toggleRecaptchaFormMessage("error", true);
+    toggleRecaptchaFormMessage("success");
+  });
+};
