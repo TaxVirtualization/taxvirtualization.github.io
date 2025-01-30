77abf276baf574a738b45292f7b875908d99b424
Mark Pinto <mark.pinto.94@gmail.com>
2025-01-30 12:21:46 -0500

    Recaptcha integration
    Another attempt at implementing recaptcha

---
+++ js/script.js
@@ -0,0 +1,29 @@
+const form = document.querySelector('form');
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const captchaResponse = grecaptcha.getResponse();
+
+    if (!captchaResponse.length > 0){
+        throw new Error("Captcha not complete");
+    }
+
+
+    const fd = new FormData(e.target);
+    const params = new URLSearchParams(fd);
+
+
+    fetch('https://docs.google.com/forms/d/e/1FAIpQLSclKHo7ZVcOUeGEe6FulYnD4AkDtiya-ycOzEOyEvvEwxQ6aQ/formResponse', {
+
+        method: "POST",
+        body: params,
+    
+    })
+
+    .then(res => res.json())
+    .then(data => console.log(data))
+    .catch(err => console.error(err))
+    
+
+});
