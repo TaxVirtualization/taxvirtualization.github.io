2e2b433d52dad5eeb51e05034c34931e473d125e
Mark Pinto <mark.pinto.94@gmail.com>
2025-01-31 20:43:15 -0500

    Removing possibly unnecessary code

--- js/script.js
+++ js/script.js
@@ -1,32 +1,39 @@
 function checkRecaptcha() 
 {
+  //used to check if captcha is verified on client end
   var response = grecaptcha.getResponse();
+  
+  //if captcha is not filled
   if(response.length == 0) 
   { 
-    //reCaptcha not verified
+    //reCaptcha not verified message is displayed
     alert("Please fill out CAPTCHA"); 
 
-      const form = document.querySelector('form');
+    //needed to prevent submission
+    const form = document.querySelector('form');
 
-      form.addEventListener('submit', (e) => {e.preventDefault();});
-      location.reload();
+    //prevents submission of form for failing to fill out CAPTCHA
+    form.addEventListener('submit', (e) => {e.preventDefault();});
+    
+    //reloads page and makes person refill form
+    location.reload();
   }
+  //else if captcha is filled
   else 
   { 
-      const form = document.querySelector('form');
-      form.addEventListener('submit', (e));
-    //reCaptch verified
-    alert("pass"); 
-      const fd = new FormData(e.target);
-    const params = new URLSearchParams(fd);
+      //const form = document.querySelector('form');
+      //form.addEventListener('submit', (e));
+
+    //const fd = new FormData(e.target);
+    //const params = new URLSearchParams(fd);
 
 
-    fetch('https://docs.google.com/forms/d/e/1FAIpQLSclKHo7ZVcOUeGEe6FulYnD4AkDtiya-ycOzEOyEvvEwxQ6aQ/formResponse', {
+    //fetch('https://docs.google.com/forms/d/e/1FAIpQLSclKHo7ZVcOUeGEe6FulYnD4AkDtiya-ycOzEOyEvvEwxQ6aQ/formResponse', {
 
-        method: "POST",
-        body: params,
+        //method: "POST",
+       // body: params,
     
-    });
+    //});
 
   }
 }
